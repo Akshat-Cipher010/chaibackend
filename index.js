563519bfb11216fedbbf07c7391664afc37c428c
Akshat-Cipher010 <akshatnigam7931@gmail.com>
2025-03-12 20:11:07 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+    "login": "Akshat-Cipher010",
+    "id": 202948021,
+    "node_id": "U_kgDODBi9tQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/202948021?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Akshat-Cipher010",
+    "html_url": "https://github.com/Akshat-Cipher010",
+    "followers_url": "https://api.github.com/users/Akshat-Cipher010/followers",
+    "following_url": "https://api.github.com/users/Akshat-Cipher010/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Akshat-Cipher010/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Akshat-Cipher010/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Akshat-Cipher010/subscriptions",
+    "organizations_url": "https://api.github.com/users/Akshat-Cipher010/orgs",
+    "repos_url": "https://api.github.com/users/Akshat-Cipher010/repos",
+    "events_url": "https://api.github.com/users/Akshat-Cipher010/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Akshat-Cipher010/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 1,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2025-03-12T11:09:25Z",
+    "updated_at": "2025-03-12T11:12:07Z"
+  }
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -15,6 +51,9 @@ app.get('/login', (req , res) => {
     res.send('<h1>Please login at chai aur code</h1>')
 })
  
+app.get('/github', (req , res) => {
+    res.json(githubData)
+})
 app.get('/youtube', (req , res) => {
     res.send('<h2>Chai aur Code</h2>')
 })
